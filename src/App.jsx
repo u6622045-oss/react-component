5f65b82d3bd48cc9421247b672a7f11e66e3410d
Botiwin Nay <u6622045@au.edu>
2025-12-19 09:43:50 +0700

    2.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import Register from './Register';
+import Greeting from './Greetings.jsx';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Register />} />
-      </Routes>
-    </Router>
+    <Greeting/>
   );
 }
 
